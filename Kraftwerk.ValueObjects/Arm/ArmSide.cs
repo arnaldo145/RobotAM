e9768c6403341ca640edf42c4440e6bcfd098bd3
Arnaldo Madeira <arnaldomadeira11@hotmail.com>
2020-03-11 14:50:15 -0300

    Ajustes na Kraftwerk API, adicionando projeto com a lógica de comunicação com a API e finalizando a implementação dos movimentos da cabeça do robô

---
+++ Kraftwerk.ValueObjects/Arm/ArmSide.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kraftwerk.ValueObjects.Arm
+{
+    public enum ArmSide
+    {
+        Left,
+        Right
+    }
+}
